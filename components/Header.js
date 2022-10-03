e12b6d3005a1f327ba679e29e952b524279b4e23
sclongyang@126.com <sclongyang@126.com>
2022-10-03 19:07:54 +0800

    header css use tailwind

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,19 @@
-import Link from "next/link";
-import { ConnectButton } from "web3uikit";
+import { ConnectButton } from "web3uikit"
+import Link from "next/link"
 
-export default function Header(){
+export default function Header() {
     return (
-        <nav>
-            <Link href="/">
-                <a>Home</a>
-            </Link>
-            <Link href="/sell-nft">
-                <a>Sell NFT</a>
-            </Link>
-            <ConnectButton moralisAuth={false} />
+        <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
+            <h1 className="py-4 px-4 font-bold text-3xl">NFT Marketplace</h1>
+            <div className="flex flex-row items-center">
+                <Link href="/">
+                    <a className="mr-4 p-6">Home</a>
+                </Link>
+                <Link href="/sell-nft">
+                    <a className="mr-4 p-6">Sell NFT</a>
+                </Link>
+                <ConnectButton moralisAuth={false} />
+            </div>
         </nav>
     )
 }
